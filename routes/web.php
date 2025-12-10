be449a3bbbc123ee4264de8a4daa5ab5c46f4ef0
deivisstasuls <deivis.stasuls@gmail.com>
2025-12-10 23:51:33 +0200

    Added nesesary routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ThreadController;
+use App\Http\Controllers\SubforumController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -24,8 +26,37 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/forum', [ThreadController::class, 'index'])
-    ->middleware(['auth', 'verified']) // Assuming only authenticated users can view the forum
-    ->name('forum.index');
+
+// Forum Routes - All require authentication
+Route::middleware(['auth', 'verified'])->group(function () {
+    // Forum Index
+    Route::get('/forum', [ThreadController::class, 'index'])->name('forum.index');
+    
+    // Subforum Routes
+    Route::get('/subforums', [SubforumController::class, 'index'])->name('subforums.index');
+    Route::get('/subforums/{slug}', [SubforumController::class, 'show'])->name('subforums.show');
+    
+    // Admin-only subforum management routes
+    // Note: Authorization is also checked in the controller
+    Route::get('/subforums/create', [SubforumController::class, 'create'])->name('subforums.create');
+    Route::post('/subforums', [SubforumController::class, 'store'])->name('subforums.store');
+    Route::get('/subforums/{slug}/edit', [SubforumController::class, 'edit'])->name('subforums.edit');
+    Route::patch('/subforums/{slug}', [SubforumController::class, 'update'])->name('subforums.update');
+    Route::delete('/subforums/{slug}', [SubforumController::class, 'destroy'])->name('subforums.destroy');
+    
+    // Thread Routes
+    Route::get('/threads/create', [ThreadController::class, 'create'])->name('threads.create');
+    Route::post('/threads', [ThreadController::class, 'store'])->name('threads.store');
+    Route::get('/threads/{slug}', [ThreadController::class, 'show'])->name('threads.show');
+    Route::get('/threads/{slug}/edit', [ThreadController::class, 'edit'])->name('threads.edit');
+    Route::patch('/threads/{slug}', [ThreadController::class, 'update'])->name('threads.update');
+    Route::delete('/threads/{slug}', [ThreadController::class, 'destroy'])->name('threads.destroy');
+    
+    // Post (Reply) Routes
+    Route::post('/threads/{threadSlug}/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('/threads/{threadSlug}/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::patch('/threads/{threadSlug}/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('/threads/{threadSlug}/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+});
     
 require __DIR__.'/auth.php';
